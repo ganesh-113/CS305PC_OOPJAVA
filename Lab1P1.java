9c0cdd679d7a04bf8d9ece5019e284c6a86752a1
ganesh-113 <mail7dganesh@gmail.com>
2025-09-03 11:29:46 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,33 @@
+class Lab1P1{
+	public static void main(String...args){
+		byte b = 15;
+		Byte bb=b;
+		
+	
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(bb.SIZE);
+		System.out.println(bb.TYPE);
+		System.out.println(bb.BYTES);	
+		
+		short s=123;
+		Short ss=s;
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.TYPE);
+		
+		int i=100;
+		Integer ii=i;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(ii.MIN_VALUE);
+		System.out.println(ii.SIZE);
+		System.out.println(ii.TYPE);
+		
+		long l=120;
+		Long ll=l;
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(ll.MIN_VALUE);
+		System.out.println(ll.SIZE);
+		System.out.println(ll.TYPE);
+		
