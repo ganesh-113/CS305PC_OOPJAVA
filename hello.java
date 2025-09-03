f0d7d1839900b43dd24881dd7d9fc3dfc5bace8b
ganesh-113 <mail7dganesh@gmail.com>
2025-09-03 09:58:10 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("i'm routine one");
+	}
+		public static void display(){
+		System.out.println("i'm a display");
+		
+		}
+}
+	
