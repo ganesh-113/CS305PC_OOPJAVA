9d32a87f2708823c68d538a75b1f0a06e092cfe9
ganesh-113 <mail7dganesh@gmail.com>
2025-10-30 22:56:18 +0530

    Update and rename P1.java to Lab1P1.java

---
+++ LAB1/Lab1P1.java
@@ -0,0 +1,66 @@
+class Lab1P1{
+	public static void main(String...args){
+		byte b = 15;
+		Byte bb = b; //Autoboxing
+		//int x = 200;
+		//byte b = x;
+		System.out.println(bb.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(bb.SIZE);
+		System.out.println(bb.TYPE);
+		System.out.println(bb.BYTES);
+		System.out.println("--------------------------Short");
+		
+		short s = 15;
+		Short ss = s; //Autoboxing
+		System.out.println(ss.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.TYPE);
+		System.out.println(ss.BYTES);
+		System.out.println("--------------------------INTEGER");
+		int i = 15;
+		Integer ii = i; //Autoboxing
+		System.out.println(ii.MAX_VALUE);
+		System.out.println(ii.MIN_VALUE);
+		System.out.println(ii.SIZE);
+		System.out.println(ii.TYPE);
+		System.out.println(ii.BYTES);
+		System.out.println("--------------------------Long");
+		long l = 15;
+		Long ll = l; //Autoboxing
+		System.out.println(ll.MAX_VALUE);
+		System.out.println(ll.MIN_VALUE);
+		System.out.println(ll.SIZE);
+		System.out.println(ll.TYPE);
+		System.out.println(ll.BYTES);
+		System.out.println("--------------------------Float");
+		float f = 15;
+		Float ff = f; //Autoboxing
+		System.out.println(ff.MAX_VALUE);
+		System.out.println(ff.MIN_VALUE);
+		System.out.println(ff.SIZE);
+		System.out.println(ff.TYPE);
+		System.out.println(ff.BYTES);
+		System.out.println("--------------------------Double");
+		double d = 15;
+		Double dd = d; //Autoboxing
+		System.out.println(dd.MAX_VALUE);
+		System.out.println(dd.MIN_VALUE);
+		System.out.println(dd.SIZE);
+		System.out.println(dd.TYPE);
+		System.out.println(dd.BYTES);
+		System.out.println("--------------------------Boolean");
+		boolean bl = true;
+		Boolean blbl= bl; //Autoboxing
+		//System.out.println(blbl.MAX_VALUE);
+		//System.out.println(blbl.MIN_VALUE);
+		//System.out.println(blbl.SIZE);
+		System.out.println(blbl.TYPE);
+		//System.out.println(blbl.BYTES);
+		
+		
+		
+	}
+
+}
