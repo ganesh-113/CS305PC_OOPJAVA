f21e72569412d1a14228b2523dd7e5bcbc787ec7
ganesh-113 <mail7dganesh@gmail.com>
2025-10-30 23:05:02 +0530

    Update Lab3P1.java

--- LAB3/Lab3P1.java
+++ LAB3/Lab3P1.java
@@ -1,16 +1,33 @@
 import java.util.Scanner;
-class Lab1P3{
+class LAB3P1{
 	public static void main(String...args){
-		Scanner s=new Scanner(System.in);
-		int a=s.nextInt();
-		int b=s.nextInt();
-		var c=a/b;
-		System.out.println(c);
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i=2;
 		
-}
-
+		while(i<n){
+			int j=2,count=0;
+			if(i==2) count++;
+			while(j<i){
+				if(i%j==0){
+					count=0;
+					break;
+				}
+				else count++;
+				j++;
+			
+			}
+			if(count>0)
+				System.out.println(i);
+				i++;
+			
 		
+		
+		}
+			
 	}
 
 
 
+
+}
